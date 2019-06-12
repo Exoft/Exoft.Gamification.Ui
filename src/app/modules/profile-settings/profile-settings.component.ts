f18e299422c7a91ae61a1b1a7fbf39e4c1ccd0ce
tanyagogina <tanyagermain23@gmail.com>
2019-06-12 14:58:01 +0300

    added user settings page logic (ability to get and edit user personal data)

--- src/app/modules/profile-settings/profile-settings.component.ts
+++ src/app/modules/profile-settings/profile-settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-profile-settings',
@@ -6,12 +9,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-settings.component.scss']
 })
 export class ProfileSettingsComponent implements OnInit {
-  public avatarSource =
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPxOAXqa96ewc_EUYvIjO6oefUTlGg1qo_AMJv7qQQlhP3vns2IA';
+  public profileSettingsForm = new FormGroup({
+    userName: new FormControl('', Validators.required),
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    emailAddress: new FormControl(
+      '',
+      Validators.compose([Validators.required, Validators.email])
+    ),
+    status: new FormControl('')
+  });
 
-  constructor() { }
+  public avatarSource: string;
+  public selectedAvatar;
 
-  ngOnInit() {
+  constructor(private userService: UserService, private location: Location) {}
+
+  public ngOnInit() {
+    const {
+      userName,
+      firstName,
+      lastName,
+      emailAddress,
+      status
+    } = this.profileSettingsForm.controls;
+
+    this.userService.getUserInfo('1').subscribe(res => {
+      this.avatarSource = res.body.avatar;
+      userName.setValue(res.body.userName);
+      firstName.setValue(res.body.firstName);
+      lastName.setValue(res.body.lastName);
+      emailAddress.setValue(res.body.emailAddress),
+        status.setValue(res.body.status);
+    });
   }
 
+  public onSelectFile(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(event.target.files[0]);
+
+      fileReader.onload = (imageLoading: Event) => {
+        this.avatarSource = fileReader.result.toString();
+        this.selectedAvatar = fileReader.result;
+      };
+    }
+  }
+
+  public onSaveChanges() {
+    if (this.profileSettingsForm.valid) {
+      const {
+        userName,
+        firstName,
+        lastName,
+        emailAddress,
+        status
+      } = this.profileSettingsForm.value;
+      const formData = {
+        avatar: this.selectedAvatar,
+        userName,
+        firstName,
+        lastName,
+        emailAddress,
+        status
+      };
+
+      this.userService.updateUserInfo(formData, '1').subscribe(
+        res => {
+          //
+        },
+        error => {
+          //
+        }
+      );
+    }
+  }
+
+  public onGoBack() {
+    this.location.back();
+  }
 }
--- src/app/modules/profile-settings/profile-settings.module.ts
+++ src/app/modules/profile-settings/profile-settings.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileSettingsRoutingModule } from './profile-settings-routing.module';
 import { AngularMaterialsModule } from 'src/app/shared/modules/angular-materials.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProfileSettingsComponent } from './profile-settings.component';
 
@@ -10,7 +11,8 @@ import { ProfileSettingsComponent } from './profile-settings.component';
   imports: [
     CommonModule,
     ProfileSettingsRoutingModule,
-    AngularMaterialsModule
+    AngularMaterialsModule,
+    ReactiveFormsModule
   ]
 })
 export class ProfileSettingsModule { }
