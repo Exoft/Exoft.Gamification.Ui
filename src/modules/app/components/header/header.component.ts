4c5f39fc8ce030bf316b99d34948bf833c55f05c
Maksym Nykolaichuk <maxwellnico@gmail.com>
2019-08-30 11:16:23 +0300

    Removed Admin tab if non-admin user

--- src/modules/app/components/header/header.component.ts
+++ src/modules/app/components/header/header.component.ts
@@ -1,13 +1,16 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
-import { menuItems } from '../../utils/constants';
-import { getFirstLettersWithSplit } from '../../utils/letterAvatar';
+import {Component, Output, EventEmitter, Input} from '@angular/core';
+import {menuItems} from '../../utils/constants';
+import {getFirstLettersWithSplit} from '../../utils/letterAvatar';
+import {OnInit} from '@angular/core/src/metadata/lifecycle_hooks';
+import {UserService} from '../../services/user.service';
+import {User} from '../../models/user';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   @Output() openSidenavEmitter = new EventEmitter();
   @Input() avatarSource: string;
   @Input() userName: string;
@@ -16,10 +19,24 @@ export class HeaderComponent {
   public menuItems = menuItems;
   public letterAvatar = getFirstLettersWithSplit;
 
+  constructor(private userService: UserService) {
+
+  }
+
+  ngOnInit(): void {
+    this.userService.getCurrentUserInfo().subscribe((u: User) => {
+      if (!u.roles.includes('Admin')) {
+        this.menuItems.pop();
+      }
+    });
+  }
+
   public openSidenav() {
     this.openSidenavEmitter.emit(null);
   }
+
   public AvatarId(avatarId: any) {
     return 'http://localhost:5000/api/files/' + avatarId;
   }
+
 }
