3759204a0ef0b55cb620ebf68617302129265aea
Orest Lehinovych <olehinovych@exoft.net>
2020-01-24 19:37:18 +0200

    added models for user, page and assign achievement

---
+++ src/modules/app/models/achievement/assign-achievement.ts
@@ -0,0 +1,9 @@
+export interface AssignAchievement {
+    id: string;
+    name: string;
+    description: string;
+    xp: number;
+    iconId: string;
+    count: number;
+    isChosen: boolean;
+}
